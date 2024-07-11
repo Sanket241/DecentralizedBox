b9638b8fecc24782af7e744c66f97eeab7d4666c
Sanket <sanketsharma241@gmail.com>
2024-07-11 12:41:00 +0530

    add some pages and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './Components/Header'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Footer from './Components/Footer'
+import Error from './Components/Error'
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/*' element={<Error/>} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Header = () => {
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap mx-auto p-3'>
+                        <img src="/logo.png" width="25px" alt="" />
+                        <span>Decentralized</span>
+                        <span>Box</span>
+
+                    </h1>
+                </Link>
+                <ul className='flex gap-4'>
+                    <Link to="/">
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                    </Link>
+                    <Link to="/about">
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                    </Link>
+                    <Link to="/signin">
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Sign</li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Header
